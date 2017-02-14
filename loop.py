1497673800e172692f48c187b7308c859364fbb5
unknown <>
2017-02-14 17:00:08 +0900

    loop.py

---
+++ loop.py
@@ -0,0 +1,19 @@
+# for, rang
+# while
+
+# for
+for num in range(1, 10): # slice [1, 2, 3][0:2]
+    print(num)
+
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for num in num_list:
+    print(num)
+
+# while 조건이 끝날때까지 계속 반복
+while True: #무한루프
+    print(1)
+
+a = 1
+while a < 10:
+    print(a)
+    a = a + 1
